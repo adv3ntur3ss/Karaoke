ba3c4d0e10cc2f8c6064bfd6c4ac7cd001c21736
Samuel Sladky <samuel.sladky@gmail.com>
2022-04-29 17:13:43 +0200

    levels done (mozno este font by som zmenil)+chybaju media querys

--- src/views/levels.vue
+++ src/views/levels.vue
@@ -1,9 +1,11 @@
 <template>
+<body class="rgb-background">
  <header class="topbanner">
           <div class="h-12 w-24">
             <h4 class="text-center p-3"><img class="logo" src="@/assets/images/logoKaraoke.png"></h4>
           </div>
         </header>
+        <h1 class="lvlh1">LEVELS</h1>
            <div class="whiteBorder">
               <div class="place">
                <button class="lvl" id="gr" @click="green()">1</button>
@@ -16,6 +18,11 @@
               <button class="play">PLAY</button>
               </div>
            </div>
+           <div class="arrowPosition">
+              <a href=""><img class="arrow" src="@/assets/images/arrow.png"></a>
+           </div>
+         <h1 class="hide">.</h1>
+      </body>
 </template>
 <script>
 

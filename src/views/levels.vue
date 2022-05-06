1e5446e17196724830f2033829d532f56e4498bc
pietro445 <pepe.polak2004@gmail.com>
2022-05-06 12:04:13 +0200

    novz commit

--- src/views/levels.vue
+++ src/views/levels.vue
@@ -4,16 +4,16 @@
         <h1 class="lvlh1">LEVELS</h1>
            <div class="whiteBorder">
               <div class="place">
-               <button class="lvl" ref="gr1" @click="green(this.$refs.gr1)">1</button>
-               <button class="lvl" ref="gr2" @click="green(this.$refs.gr2)">2</button>
-               <button class="lvl" ref="gr3" @click="green(this.$refs.gr3)">3</button>
-               <button class="lvl" ref="gr4" @click="green(this.$refs.gr4)">4</button>
-               <button class="lvl" ref="gr5" @click="green(this.$refs.gr5)">5</button>
-               <button class="lvl" ref="gr6" @click="green(this.$refs.gr6)">6</button>
-               <button class="lvl" ref="gr7" @click="green(this.$refs.gr7)">7</button>
-               <button class="lvl" ref="gr8" @click="green(this.$refs.gr8)">8</button>
-               <button class="lvl" ref="gr9" @click="green(this.$refs.gr9)">9</button>
-               <button class="lvl tablet" ref="gr10" @click="green(this.$refs.gr10)">10</button>
+               <button class="level" ref="gr1" @click="green(this.$refs.gr1)">1</button>
+               <button class="level" ref="gr2" @click="green(this.$refs.gr2)">2</button>
+               <button class="level" ref="gr3" @click="green(this.$refs.gr3)">3</button>
+               <button class="level" ref="gr4" @click="green(this.$refs.gr4)">4</button>
+               <button class="level" ref="gr5" @click="green(this.$refs.gr5)">5</button>
+               <button class="level" ref="gr6" @click="green(this.$refs.gr6)">6</button>
+               <button class="level" ref="gr7" @click="green(this.$refs.gr7)">7</button>
+               <button class="level" ref="gr8" @click="green(this.$refs.gr8)">8</button>
+               <button class="level" ref="gr9" @click="green(this.$refs.gr9)">9</button>
+               <button class="level tablet" ref="gr10" @click="green(this.$refs.gr10)">10</button>
               </div>
               <div class="center">
               <button class="play-button">PLAY</button>
